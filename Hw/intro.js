856ba63477d05159eaed439ee4717da3c528197d
Shamkhal44 <shamkhal.code.edu.az>
2021-05-22 11:13:03 +0400

    hw2

---
+++ Hw/intro.js
@@ -0,0 +1,12 @@
+var a; 
+a = 5; 
+
+var b = 13; 
+
+
+b = 14 
+var c = a + b 
+
+
+
+console.log(c)
---
+++ task2.js
@@ -0,0 +1,16 @@
+var a = 3333;
+console.log(a);
+
+a = 7*10000 + a;
+console.log(a);
+
+a = a * 10 + 8;
+
+console.log(a);
+
+
+var a = 333
+
+
+a = a * 1000000 + 333333
+console.log(a);
